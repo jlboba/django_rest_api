8f434d0ce424366d02fbb3fd4323a43f6363f282
Jerrica Bobadilla <jerrica@Jerricas-iMac-5.home>
2020-10-19 13:51:44 -0700

    fix permissions

--- django_rest_api/settings.py
+++ django_rest_api/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'afternoon-escarpment-71350.herokuapp.com']
+ALLOWED_HOSTS = ['localhost']
 
 # Application definition
 
